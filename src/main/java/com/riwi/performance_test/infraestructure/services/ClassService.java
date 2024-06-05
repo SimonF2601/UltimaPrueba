bcb06eec27be93dad2e3ff169581819ced559e3a
Simón Franco <simonfranco2005@gmail.com>
2024-06-05 11:36:49 -0500

    insert class

--- src/main/java/com/riwi/performance_test/infraestructure/services/ClassService.java
+++ src/main/java/com/riwi/performance_test/infraestructure/services/ClassService.java
@@ -1,80 +1,67 @@
-// package com.riwi.performance_test.infraestructure.services;
+package com.riwi.performance_test.infraestructure.services;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.data.domain.Page;
-// import org.springframework.data.domain.PageRequest;
-// import org.springframework.data.domain.Sort;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
-// import com.riwi.performance_test.api.dtos.request.StudentRequest;
-// import com.riwi.performance_test.api.dtos.response.StudentResponse;
-// import com.riwi.performance_test.domain.entities.StudentEntity;
-// import com.riwi.performance_test.domain.repositories.ClassRepository;
-// import com.riwi.performance_test.domain.repositories.StudentRepository;
-// import com.riwi.performance_test.infraestructure.abstract_services.IClassService;
-// import com.riwi.performance_test.infraestructure.helpers.ServiceHelper;
-// import com.riwi.performance_test.infraestructure.helpers.mappers.ClassMapper;
-// import com.riwi.performance_test.infraestructure.helpers.mappers.StudentMapper;
-// import com.riwi.performance_test.utils.enums.SortType;
+import com.riwi.performance_test.api.dtos.request.ClassRequest;
+import com.riwi.performance_test.api.dtos.request.StudentRequest;
+import com.riwi.performance_test.api.dtos.response.ClassResponse;
+import com.riwi.performance_test.api.dtos.response.StudentResponse;
+import com.riwi.performance_test.domain.entities.ClassEntity;
+import com.riwi.performance_test.domain.entities.StudentEntity;
+import com.riwi.performance_test.domain.repositories.ClassRepository;
+import com.riwi.performance_test.domain.repositories.StudentRepository;
+import com.riwi.performance_test.infraestructure.abstract_services.IClassService;
+import com.riwi.performance_test.infraestructure.helpers.ServiceHelper;
+import com.riwi.performance_test.infraestructure.helpers.mappers.ClassMapper;
+import com.riwi.performance_test.infraestructure.helpers.mappers.StudentMapper;
+import com.riwi.performance_test.utils.enums.SortType;
 
-// import lombok.AllArgsConstructor;
+import lombok.AllArgsConstructor;
 
-// @Service
-// @AllArgsConstructor
-// public class ClassService {
+@Service
+@AllArgsConstructor
+public class ClassService implements IClassService{
 
-//     private final IClassService service;
+    private final IClassService service;
 
-//     @Autowired
-//     private final ClassMapper classMapper;
-//     @Autowired
-//     private final ClassRepository classRepository;
-//     @Autowired
-//     private final ServiceHelper serviceHelper;
+    @Autowired
+    private final ClassMapper classMapper;
+    @Autowired
+    private final ClassRepository classRepository;
+    @Autowired
+    private final ServiceHelper serviceHelper;
 
-//     @Override
-//     public StudentResponse create(StudentRequest request) {
-//         StudentEntity student = this.studentMapper.requestToEntity(request);
-//         return this.studentMapper.entityToResponse(this.studentRepository.save(student));
-//     }
+    @Override
+    public ClassResponse create(ClassRequest request) {
+        ClassEntity objClass = this.classMapper.requestToEntity(request);
+        return this.classMapper.entityToResponse(this.classRepository.save(objClass));
+    }
 
-//     @Override
-//     public StudentResponse get(Long id) {
-//         return this.studentMapper.entityToResponse(this.serviceHelper.find(id, studentRepository, "student"));
-//     }
+    @Override
+    public ClassResponse get(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
 
-//     @Override
-//     public StudentResponse update(StudentRequest request, Long id) {
-//         StudentEntity student = this.serviceHelper.find(id, studentRepository, "student");
+    @Override
+    public ClassResponse update(ClassRequest request, Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
 
-//         student = this.studentMapper.requestToEntity(request);
-//         student.setId(id);
- 
-//         return this.studentMapper.entityToResponse(this.studentRepository.save(student));
-//     }
+    @Override
+    public void delete(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
 
-//     @Override
-//     public void delete(Long id) {
-//         // TODO Auto-generated method stub
-//         throw new UnsupportedOperationException("Unimplemented method 'delete'");
-//     }
-
-//     @Override
-//     public Page<StudentResponse> getAll(int page, int size, SortType sort) {
-//         if (page <0) page = 0;
-
-//         PageRequest pagination = null;
-
-//         switch (sort) {
-//             case NONE -> pagination = PageRequest.of(page, size);
-    
-//             case ASC -> pagination = PageRequest.of(page, size, Sort.by(FIELD_BY_SORT).ascending());
-            
-//             case DESC -> pagination = PageRequest.of(page, size, Sort.by(FIELD_BY_SORT).descending());  
-//         }
-
-//         return this.studentRepository.findAll(pagination)
-//                 .map(student -> this.studentMapper.entityToResponse(student));
-//     }
-
-// }
+    @Override
+    public Page<ClassResponse> getAll(int page, int size, SortType sort) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+}
